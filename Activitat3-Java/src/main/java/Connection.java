ed4c86bd4a52ebd599e92f90d398d29539d7fbb6
Oriol Carulla <04ocarulla@gmail.com>
2025-02-03 17:16:10 +0100

    clear the logs

--- Activitat3-Java/src/main/java/Connection.java
+++ Activitat3-Java/src/main/java/Connection.java
@@ -22,10 +22,8 @@ public class Connection {
                 .serverApi(serverApi)
                 .build();
 
-        // Create a new client and connect to the server
         try (MongoClient mongoClient = MongoClients.create(settings)) {
             try {
-                // Send a ping to confirm a successful connection
                 MongoDatabase database = mongoClient.getDatabase("admin");
                 database.runCommand(new Document("ping", 1));
                 System.out.println("Pinged your deployment. You successfully connected to MongoDB!");
